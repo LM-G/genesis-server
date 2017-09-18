4e9498790eac022db7e263de2d6ff0c314de5bef
Solofeed <louismarie.guillemot@gmail.com>
2017-09-18 20:33:58 +0200

    initial commit

---
+++ src/main/java/com/solofeed/GenesisServerApplication.java
@@ -0,0 +1,12 @@
+package com.solofeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesisServerApplication.class, args);
+	}
+}
